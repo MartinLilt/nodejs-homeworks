d0b61c2ea89983e8e16c58483341f687dc5513b6
MartinLilt <80705619+MartinLilt@users.noreply.github.com>
2022-05-08 14:27:42 +0300

    work-8 FN

--- work-8/app.js
+++ work-8/app.js
@@ -1,25 +1,30 @@
 // 3Oxti67oogYOsbDS
 const mongoose = require('mongoose');
-const { DB_HOST } = require('./config');
-
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+const productsRouter = require('./routes/api/products');
+
 const app = express();
-mongoose
-  .connect(DB_HOST)
-  .then(() => console.log('Database connect'))
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use('/api/products', productsRouter);
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT, () => console.log('Server running..')))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
